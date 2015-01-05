9a5e306ea4777cf3aba510c025083f73f3fa0951
mondaysafterschool <coldplayfan246@gmail.com>
2015-01-05 16:15:17 -0500

    Create joshua vera.py

---
+++ joshua vera.py
@@ -0,0 +1,14 @@
+#==========imports=====================#
+import sys
+import os
+#======================================#
+#questionare!#
+x= [50,51,52,53,54,55,56,57,58,59,60]
+question1 = raw_input "Hello what is your name?"
+question2 = input "What is your age?"
+print "thats a cool name"+ question1
+print "Wow your that old?!"+ question2
+for x in range(50,60):
+  print "well your a little older than i thought..."
+#Easter egg#
+if question2 > "9000" print "ITS OVER 9000!!!"
