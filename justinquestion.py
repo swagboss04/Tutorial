60a6195d2d2e8cd8b2ad2c2c153c523a12eb83e1
swagboss04 <justinharrison151@gmail.com>
2015-01-24 10:49:18 -0500

    Create justinquestion.py

---
+++ justinquestion.py
@@ -0,0 +1,27 @@
+import os
+import sys
+q1=raw_imput"What is your favorite color red or blue"
+if q1== red:
+  print 'i like that too'
+else:
+  print"blue is amazing"
+q2=raw_imut"What is your favorite subject writing or math"
+if q2== writing:
+  print'i like that too'
+else:
+  print'math is stupid'
+q3=raw_imput"What is your favorite year of high school freshman or senior"
+if q3== senior:
+  print'i like that too'
+else:
+  print'freshman year was the worst'
+q4=raw_imput"What do you like to do for fun run or sleep"
+if q4== sleep
+  print'i like that too'
+else:
+  print' running is too much'
+q5=raw_imput"What do you like baseball or football"
+if q5== baseball:
+  print 'I like that too'
+else:
+  print"football is cool"
